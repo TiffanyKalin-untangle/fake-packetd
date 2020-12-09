2cbda334350408f461455653e1a545cb36d50cf3
Tiffany Kalin <tkalin@untangle.com>
2020-12-09 11:15:05 -0700

    Basic scaffolding for fake-packetd

--- cmd/fake-packetd/fake-packetd.go
+++ cmd/fake-packetd/fake-packetd.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/untangle/packetd/services/logger"
+	"github.com/TiffanyKalin-untangle/fake-packetd/services/fake-restd"
 )
 
+var shutdownFlag bool
+
 func main() {
+	startServices()
+
+	handleSignals()
+
+	for !getShutdown() {
+		select {
+		case <-time.After(2 * time.Second):
+			logger.Debug("fake-packetd is running...\n")
+		}
+	}
+
+	stopServices()
+	logger.Info("Shutdown fake-packetd...\n")
+}
+
+func startServices() {
+	setIsShutdown(false)
 	logger.Startup()
-	logger.Info("fake-packetd staring up...\n")
+	fake_restd.Startup()
+	logger.Info("fake-packetd starting up...\n")
+}
+
+func stopServices() {
+	logger.Shutdown()
+}
+
+func setIsShutdown(flag bool) {
+	shutdownFlag = flag
+}
+
+func getShutdown() bool {
+	return shutdownFlag
+}
+
+func handleSignals() {
+	// Add SIGINT & SIGTERM handler (exit)
+	termch := make(chan os.Signal, 1)
+	signal.Notify(termch, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-termch
+		go func() {
+			logger.Info("Received signal [%v]. Setting shutdown flag\n", sig)
+			setIsShutdown(true)
+		}()
+	}()
 }
---
+++ services/fake-restd/fake-restd.go
@@ -0,0 +1,9 @@
+package fake_restd
+
+import (
+	"github.com/untangle/packetd/services/logger"
+)
+
+func Startup() {
+	logger.Info("Starting fake-restd...\n")
+}
