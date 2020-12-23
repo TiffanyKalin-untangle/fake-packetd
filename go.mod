426e3be688e8244cd5a7d309179322def097952b
Tiffany Kalin <tkalin@untangle.com>
2020-12-23 14:58:26 -0700

    MFW-1178: use interface for zmq server so can be used multiple places

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/TiffanyKalin-untangle/fake-packetd
 go 1.12
 
 require (
-	github.com/pebbe/zmq4 v1.2.2
-	github.com/untangle/golang-shared v0.0.0-20201223182702-8f34fda60b52
+	github.com/pebbe/zmq4 v1.2.2 // indirect
+	github.com/untangle/golang-shared v0.0.0-20201223215520-4ba782916042
 	google.golang.org/protobuf v1.25.0
 )
--- services/zmqd/zmqd.go
+++ services/zmqd/zmqd.go
@@ -2,82 +2,27 @@ package zmqd
 
 import (
 	"errors"
-	"sync"
-	"syscall"
-	"time"
 
-	zmq "github.com/pebbe/zmq4"
-	"github.com/untangle/golang-shared/services/logger"
 	"github.com/TiffanyKalin-untangle/fake-packetd/services/dispatch"
-	zreq "github.com/untangle/golang-shared/structs/protocolbuffers/ZMQRequest"
+	rzs "github.com/untangle/golang-shared/services/restdZmqServer"
 	prep "github.com/untangle/golang-shared/structs/protocolbuffers/PacketdReply"
+	zreq "github.com/untangle/golang-shared/structs/protocolbuffers/ZMQRequest"
 	"google.golang.org/protobuf/proto"
 	spb "google.golang.org/protobuf/types/known/structpb"
 )
 
-var isShutdown = make(chan struct{})
-var wg sync.WaitGroup
+type packetdProc int 
 
 func Startup() {
-	logger.Info("Starting zmq service...\n")
-	socketServer()
-
+	processer := packetdProc(0)
+	rzs.Startup(processer)
 }
 
 func Shutdown() {
-	close(isShutdown)
-	wg.Wait()
+	rzs.Shutdown()
 }
 
-func socketServer() {
-	zmqSocket, err := zmq.NewSocket(zmq.REP)
-	if err != nil {
-		logger.Warn("Failed to create zmq socket...", err)
-	}
-
-	zmqSocket.Bind("tcp://*:5555")
-	wg.Add(1)
-	go func(waitgroup *sync.WaitGroup, socket *zmq.Socket) {
-		defer socket.Close()
-		defer waitgroup.Done()
-		tick := time.Tick(500 * time.Millisecond)
-		for {
-			select {
-			case <-isShutdown:
-				logger.Info("Shutdown is seen\n")
-				return
-			case <-tick:
-				logger.Info("Listening for requests\n")
-				requestRaw, err := socket.RecvMessageBytes(zmq.DONTWAIT)
-				if err != nil {
-					if zmq.AsErrno(err) != zmq.AsErrno(syscall.EAGAIN) {
-						logger.Warn("Error on receive ", err, "\n")
-					}
-					continue
-				}
-
-				// Process message
-				request := &zreq.ZMQRequest{}
-				if err := proto.Unmarshal(requestRaw[0], request); err != nil {
-					logger.Warn("Error on unmasharlling ", err, "\n")
-					continue
-				}
-				logger.Info("Received ", request, "\n")
-
-				reply, err := processMessage(request)
-				if err != nil {
-					logger.Warn("Error on processing reply: ", err, "\n")
-					continue
-				}
-
-				socket.SendMessage(reply)
-				logger.Info("Sent ", reply, "\n")
-			}
-		} 
-	}(&wg, zmqSocket)
-}
-
-func processMessage(request *zreq.ZMQRequest) (processedReply []byte, processErr error) {
+func (p packetdProc) Process(request *zreq.ZMQRequest) (processedReply []byte, processErr error) {
 	function := request.Function
 	reply := &prep.PacketdReply{}
 
